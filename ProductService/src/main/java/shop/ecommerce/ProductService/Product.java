3b05d42883dda6ffb89440b6335d2f3fa20dc68c
susumintraaa <neem_za106@hotmail.com>
2018-11-11 23:06:16 +0700

    [CREATE] สร้าง Product model

--- ProductService/src/main/java/shop/ecommerce/ProductService/Product.java
+++ ProductService/src/main/java/shop/ecommerce/ProductService/Product.java
@@ -1,8 +1,77 @@
 package shop.ecommerce.ProductService;
 
+import java.util.Locale.Category;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.boot.archive.scan.spi.Categorization;
 
 @Entity
+@Table(name="shops")
 public class Product {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    private String sku;
+    private String name;
+    private Category category;
+    private double price;
+    private String detail;
+    private String thunbnail;
+
+
+    public String getSku() {
+        return this.sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDetail() {
+        return this.detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getThunbnail() {
+        return this.thunbnail;
+    }
+
+    public void setThunbnail(String thunbnail) {
+        this.thunbnail = thunbnail;
+    }
+
+
 
 }
