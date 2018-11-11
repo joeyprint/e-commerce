dd4c2d3b8128bdb982672d406631d82c2edeb0e9
bazsup <supawit.ruen@mail.kmutt.ac.th>
2018-11-11 23:32:47 +0700

    [ADD] FrontendService

---
+++ FrontendService/src/main/java/th/in/shopdi/FrontendService/FrontendServiceApplication.java
@@ -0,0 +1,12 @@
+package th.in.shopdi.FrontendService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendServiceApplication.class, args);
+	}
+}
