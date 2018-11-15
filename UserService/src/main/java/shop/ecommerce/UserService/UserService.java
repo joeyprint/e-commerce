b091a5e6675c9fed75ccbc7086cd113ee7059857
wilawannun <wilawan.33@hotmail.com>
2018-11-15 15:46:06 +0700

    [UPDATE] เพิ่ม method addAddress()

--- UserService/src/main/java/shop/ecommerce/UserService/UserService.java
+++ UserService/src/main/java/shop/ecommerce/UserService/UserService.java
@@ -3,11 +3,17 @@ package shop.ecommerce.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserService {
+    
+    
 
     @Autowired
     private UserRepository userRepository;
 
     public User updateAddress(User user) {
+        return userRepository.save(user);
+    }
+    
+    public User addAddress(User user){
         return userRepository.save(user);
     }
 }
