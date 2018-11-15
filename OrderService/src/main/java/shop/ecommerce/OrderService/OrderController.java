e982798b5c377a6c8e1f12b35ee0161a81b32947
wilawannun <wilawan.33@hotmail.com>
2018-11-15 17:09:57 +0700

    [UPDATE] เพิ่ม method createOrder() บน orderController

--- OrderService/src/main/java/shop/ecommerce/OrderService/OrderController.java
+++ OrderService/src/main/java/shop/ecommerce/OrderService/OrderController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import shop.ecommerce.OrderService.OrderService;
+
 import javax.validation.Valid;
 import java.util.List;
 
@@ -21,5 +23,14 @@ public class OrderController {
         Order getOrder = orderService.getOrder(orderId);
         return new ResponseEntity<Order>(getOrder, HttpStatus.OK);
     }
+
+    @PostMapping("/order/create"){
+    public ResponseEntity<Order> createOrder(Order order){
+        Order createOrder = orderService.createOrder(order);
+        return new  ResponseEntity<Order>(createOrder, HttpStatus.OK);
+    }
+    
+
+    
 
 }
