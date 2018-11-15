e597822eb2e373fecdf51493ad3fa3b6b514550b
mingzkun <kunlakarn.t@mail.kmutt.ac.th>
2018-11-15 15:29:36 +0700

    [ADD] updateUser() on UserService

--- UserService/src/main/java/shop/ecommerce/UserService/UserService.java
+++ UserService/src/main/java/shop/ecommerce/UserService/UserService.java
@@ -1,5 +1,13 @@
 package shop.ecommerce.UserService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class UserService {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    public User updateAddress(User user) {
+        return userRepository.save(user);
+    }
 }
