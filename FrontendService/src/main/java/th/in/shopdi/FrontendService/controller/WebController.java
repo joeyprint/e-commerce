af69a6fb4466fa012ae16dbf5dffb21153b8f099
bazsup <supawit.ruen@mail.kmutt.ac.th>
2018-11-15 13:33:38 +0700

    [UPDATE] webcontroller home refactor use product constructor

--- FrontendService/src/main/java/th/in/shopdi/FrontendService/controller/WebController.java
+++ FrontendService/src/main/java/th/in/shopdi/FrontendService/controller/WebController.java
@@ -3,6 +3,7 @@ package th.in.shopdi.FrontendService.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import th.in.shopdi.FrontendService.model.Product;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,8 +19,11 @@ public class WebController {
   public String home(Model model) {
     List<Product> productList = new ArrayList<>();
     for (int i = 0; i < 10; i++) {
-      productList.add(new Product("sneaker", 2000,
-          "https://static.highsnobiety.com/wp-content/uploads/2015/12/03162839/best-sneaker-featured.jpg"));
+      Product product = new Product();
+      product.setProductname("sneaker");
+      product.setPrice(2000);
+      product.setImageURL("https://static.highsnobiety.com/wp-content/uploads/2015/12/03162839/best-sneaker-featured.jpg");
+      productList.add(product);
     }
     model.addAttribute("products", productList);
     return "index";
