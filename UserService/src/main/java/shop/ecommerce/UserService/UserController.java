a9d8042f4762f7153837e21d4fd5847ea5caa0bb
mingzkun <kunlakarn.t@mail.kmutt.ac.th>
2018-11-15 16:00:51 +0700

    [UPDATE] add updateAddress() on UserController

--- UserService/src/main/java/shop/ecommerce/UserService/UserController.java
+++ UserService/src/main/java/shop/ecommerce/UserService/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +15,14 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/user/{id}")
-    public ResponseEntity<User> viewUserDetail(@PathVariable (name = "id") long userId){
+    public ResponseEntity<User> viewUserDetail(@PathVariable(name = "id") long userId) {
         User user_object = UserService.viewUserDetai(userId);
+        return new ResponseEntity<User>(user_object, HttpStatus.OK);
+    }
+
+    @PostMapping("/user/{id}/address")
+    public ResponseEntity<User> updateAddress(@PathVariable(name = "id") long userId) {
+        User user_object = UserService.updateAddress(userId, address);
         return new ResponseEntity<User>(user_object, HttpStatus.OK);
     }
 
