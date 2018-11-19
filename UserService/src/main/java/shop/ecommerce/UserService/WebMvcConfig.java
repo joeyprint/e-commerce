2fc9fbc3cfb88a8d5de7bae4115be74d993eed83
bazsup <supawit.ruen@mail.kmutt.ac.th>
2018-11-19 22:28:01 +0700

    [CREATE] WebMvcConfig for add Interceptor (filter, middleware) for route /test/**

---
+++ UserService/src/main/java/shop/ecommerce/UserService/WebMvcConfig.java
@@ -0,0 +1,30 @@
+package shop.ecommerce.UserService;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.MappedInterceptor;
+import shop.ecommerce.UserService.interceptor.TokenInterceptor;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+  @Value("${authenservice.jwt.secret}")
+  private String jwtSecret;
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new TokenInterceptor(jwtSecret))
+            .addPathPatterns(new String[] {"/test/**"});
+//    registry.addInterceptor(myInterceptor());
+  }
+
+  @Bean
+  public MappedInterceptor myInterceptor() {
+    return new MappedInterceptor(
+            new String[] {"/test/**"},
+            new TokenInterceptor(jwtSecret));
+  }
+}
