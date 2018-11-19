c1ef3f03b356bf5fd1486d3d88a246d94164a4c5
bazsup <supawit.ruen@mail.kmutt.ac.th>
2018-11-19 22:33:57 +0700

    [DELETE] unused facebookAdapter from UserService inside FrontendService

--- FrontendService/src/main/java/th/in/shopdi/FrontendService/service/UserService.java
+++ FrontendService/src/main/java/th/in/shopdi/FrontendService/service/UserService.java
@@ -1,20 +1,14 @@
 package th.in.shopdi.FrontendService.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import th.in.shopdi.FrontendService.DTO.FacebookAccount;
-import th.in.shopdi.FrontendService.DTO.User;
-import th.in.shopdi.FrontendService.adapter.AuthenFacebookAdapter;
 import th.in.shopdi.FrontendService.adapter.UserAdapter;
 
 @Service
 public class UserService {
-  private AuthenFacebookAdapter authenFacebookAdapter;
 
   private UserAdapter userAdapter;
 
   public UserService() {
-    authenFacebookAdapter = new AuthenFacebookAdapter();
     userAdapter = new UserAdapter();
   }
 
