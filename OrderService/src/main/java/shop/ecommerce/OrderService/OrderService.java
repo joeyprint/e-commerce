d54267166696bdd6a140f79948e850a1a4c68f1f
PimIsariya <pim.paw.pitode@gmail.com>
2018-11-19 15:56:03 +0700

    [UPDATE] แก้ไข method createOrder ให้รับค่าของ userId และ productId

--- OrderService/src/main/java/shop/ecommerce/OrderService/OrderService.java
+++ OrderService/src/main/java/shop/ecommerce/OrderService/OrderService.java
@@ -3,17 +3,28 @@ package shop.ecommerce.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import shop.ecommerce.ProductAdapter.ProductAdapter;
+import shop.ecommerce.UserAdapter.UserAdapter;
+
 @Service
 public class OrderService {
 
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private UserAdapter userAdapter;
+
+    @Autowired
+    private ProductAdapter productAdapter;
+
     public Order getOrder(long id) {
         return orderRepository.findById(id);
     }
 
-    public Order createOrder(Order order) {
+    public Order createOrder(long userId, long productId, Order order) {
+        order.setUser(userAdapter.getUserDetail(userId));
+        order.setProduct(productAdapter.getProductDetail(productId));
         return orderRepository.save(order);
     }
 
