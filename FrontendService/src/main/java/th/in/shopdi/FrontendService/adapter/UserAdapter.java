a73cf42d3ffd70c3bd0d00a60b1f5773b661ab2c
bazsup <supawit.ruen@mail.kmutt.ac.th>
2018-11-20 21:27:04 +0700

    [ADD] userAdapter getProfileByToken

--- FrontendService/src/main/java/th/in/shopdi/FrontendService/adapter/UserAdapter.java
+++ FrontendService/src/main/java/th/in/shopdi/FrontendService/adapter/UserAdapter.java
@@ -1,14 +1,26 @@
 package th.in.shopdi.FrontendService.adapter;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import th.in.shopdi.FrontendService.DTO.User;
 
+@Service
 public class UserAdapter {
-  public User findOrCreate(User user) {
+
+  @Value("${user.service.url}")
+  private String userServiceURL;
+
+  public User getProfileByToken(String token) {
     RestTemplate restTemplate = new RestTemplate();
-//    @Value("${userservice.url}")
-    String url = "http://localhost:8081";
-    System.out.println("get user adpter");
-    return restTemplate.postForObject(url + "/user", user, User.class);
+    String url = userServiceURL + "/me";
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("Authorization", token);
+    HttpEntity<String> request = new HttpEntity<String>(headers);
+
+    ResponseEntity<User> response = restTemplate.exchange(url, HttpMethod.GET, request, User.class);
+    return response.getBody();
   }
 }
