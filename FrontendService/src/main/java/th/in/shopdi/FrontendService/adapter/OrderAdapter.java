545d8598bf8e5048d6b2eba2d70b8b6fd3ee5604
bazsup <supawit.ruen@mail.kmutt.ac.th>
2018-11-20 23:36:52 +0700

    [UPDATE] OrderAdapter in FrontendService call other Adapter for mapping

--- FrontendService/src/main/java/th/in/shopdi/FrontendService/adapter/OrderAdapter.java
+++ FrontendService/src/main/java/th/in/shopdi/FrontendService/adapter/OrderAdapter.java
@@ -1,10 +1,14 @@
 package th.in.shopdi.FrontendService.adapter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import th.in.shopdi.FrontendService.DTO.Order;
+import th.in.shopdi.FrontendService.DTO.OrderMapObject;
+import th.in.shopdi.FrontendService.DTO.Product;
+import th.in.shopdi.FrontendService.DTO.User;
 
 @Service
 public class OrderAdapter {
@@ -12,10 +16,25 @@ public class OrderAdapter {
     @Value("${order.service.url}")
     private String orderServiceURL;
 
-    public Order getOrderDetail(long orderId){
+    @Autowired
+    private ProductAdapter productAdapter;
+
+    @Autowired
+    private UserAdapter userAdapter;
+
+    public OrderMapObject getOrderDetail(long orderId){
         RestTemplate restTemplate = new RestTemplate();
         String url = orderServiceURL+"/order/"+orderId;
         Order order = restTemplate.getForObject(url, Order.class);
-        return order;
+
+        Product product = productAdapter.getProductDetail(order.getProductId());
+        User user = userAdapter.getUserDetail(order.getUserId());
+
+        OrderMapObject orderWithObject = new OrderMapObject();
+        orderWithObject.setId(orderId);
+        orderWithObject.setCreateAt(order.getCreateAt());
+        orderWithObject.setProduct(product);
+        orderWithObject.setUser(user);
+        return orderWithObject;
     }
 }
