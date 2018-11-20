e448d6c4c00db66da7084b3b8bb8538df302eb24
bazsup <supawit.ruen@mail.kmutt.ac.th>
2018-11-20 22:31:36 +0700

    [UPDATE] Payment add entity table `payments` update attributes

--- OrderService/src/main/java/shop/ecommerce/OrderService/model/Payment.java
+++ OrderService/src/main/java/shop/ecommerce/OrderService/model/Payment.java
@@ -1,23 +1,85 @@
 package shop.ecommerce.OrderService.model;
 
-public class Payment {
+import javax.persistence.*;
+import java.io.Serializable;
 
-  private String token;
-  private long amount;
+@Entity
+@Table(name = "payments")
+public class Payment implements Serializable {
 
-  public String getToken() {
-    return token;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  private String holderName;
+
+  @Column(length = 16)
+  private String cardNumber;
+
+  @Column(length = 2)
+  private String expiredMonth;
+
+  @Column(length = 2)
+  private String expiredYear;
+
+  @Column(length = 3)
+  private String cvv;
+
+  private long userId;
+
+  public String getCvv() {
+    return cvv;
   }
 
-  public void setToken(String token) {
-    this.token = token;
+  public void setCvv(String cvv) {
+    this.cvv = cvv;
   }
 
-  public long getAmount() {
-    return amount;
+  public long getUserId() {
+    return userId;
   }
 
-  public void setAmount(long amount) {
-    this.amount = amount;
+  public void setUserId(long userId) {
+    this.userId = userId;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getHolderName() {
+    return holderName;
+  }
+
+  public void setHolderName(String holderName) {
+    this.holderName = holderName;
+  }
+
+  public String getCardNumber() {
+    return cardNumber;
+  }
+
+  public void setCardNumber(String cardNumber) {
+    this.cardNumber = cardNumber;
+  }
+
+  public String getExpiredMonth() {
+    return expiredMonth;
+  }
+
+  public void setExpiredMonth(String expiredMonth) {
+    this.expiredMonth = expiredMonth;
+  }
+
+  public String getExpiredYear() {
+    return expiredYear;
+  }
+
+  public void setExpiredYear(String expiredYear) {
+    this.expiredYear = expiredYear;
   }
 }
