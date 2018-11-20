25977ef30da090094a35c90c61830f691bd5bbbd
bazsup <supawit.ruen@mail.kmutt.ac.th>
2018-11-20 14:58:03 +0700

    [CREATE] getAllProduct method in ProductAdapter inside FrontendService

--- FrontendService/src/main/java/th/in/shopdi/FrontendService/adapter/ProductAdapter.java
+++ FrontendService/src/main/java/th/in/shopdi/FrontendService/adapter/ProductAdapter.java
@@ -6,11 +6,20 @@ import org.springframework.web.client.RestTemplate;
 
 import th.in.shopdi.FrontendService.DTO.Product;
 
+import java.util.List;
+
 @Service
 public class ProductAdapter {
 
     @Value("${product.service.url}")
     private String productServiceURL;
+
+    public List<Product> getAllProduct(){
+        RestTemplate restTemplate = new RestTemplate();
+        String url = productServiceURL + "/products";
+        List<Product> products = restTemplate.getForObject(url, List.class);
+        return products;
+    }
 
     public Product getProductDetail(long id){
         RestTemplate restTemplate = new RestTemplate();
