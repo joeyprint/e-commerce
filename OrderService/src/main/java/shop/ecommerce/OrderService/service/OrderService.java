f24a11965617631a3ff28f2b69b2fb2fc3b0bdb4
PimIsariya <pim.paw.pitode@gmail.com>
2018-11-20 16:54:06 +0700

    REVERSE COMMIT

---
+++ OrderService/src/main/java/shop/ecommerce/OrderService/service/OrderService.java
@@ -0,0 +1,27 @@
+package shop.ecommerce.OrderService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import shop.ecommerce.ProductAdapter.ProductAdapter;
+import shop.ecommerce.UserAdapter.UserAdapter;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    private UserAdapter userAdapter;
+    private ProductAdapter productAdapter;
+
+    public Order getOrder(long id) {
+        return orderRepository.findById(id);
+    }
+
+    public Order createOrder(long userId, long productId, Order order) {
+        order.setUser(userAdapter.getUserDetail(userId));
+        order.setProduct(productAdapter.getProductDetail(productId));
+        return orderRepository.save(order);
+    }
+
+}
