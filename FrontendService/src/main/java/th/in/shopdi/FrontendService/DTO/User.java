9399382db0fe84a4ba216ce1a81319e010466d54
bazsup <supawit.ruen@mail.kmutt.ac.th>
2018-11-20 23:49:35 +0700

    [UPDATE] User DTO (Data to Object) in FrontendService add attribute id

--- FrontendService/src/main/java/th/in/shopdi/FrontendService/DTO/User.java
+++ FrontendService/src/main/java/th/in/shopdi/FrontendService/DTO/User.java
@@ -1,6 +1,8 @@
 package th.in.shopdi.FrontendService.DTO;
 
 public class User {
+  private long id;
+
   private String facebookId;
 
   private String firstname;
@@ -12,6 +14,14 @@ public class User {
   private String address;
 
   private String telNo;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
 
   public String getTelNo() {
     return telNo;
