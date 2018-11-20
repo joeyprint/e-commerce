06010671e2bcd0cae1f3dba7f304da46e3166b61
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-11-20 16:38:33 +0700

    [UPDATE] update model order

--- OrderService/src/main/java/shop/ecommerce/OrderService/Order.java
+++ OrderService/src/main/java/shop/ecommerce/OrderService/Order.java
@@ -4,25 +4,40 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Locale.Category;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import shop.ecommerce.ProductAdapter.Product;
 import shop.ecommerce.UserAdapter.User;
 
 @Entity
 @Table(name = "orders")
+@EntityListeners(AuditingEntityListener.class)
 public class Order implements Serializable {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    
     private Product product;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name="create_at")
     private Date createAt;
+
     private User user;
 
     public Order() {
