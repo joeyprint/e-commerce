a54f51c31f3ad2db01b1fb7c0bab9d48190d0889
bazsup <supawit.ruen@mail.kmutt.ac.th>
2018-11-20 23:50:00 +0700

    [UPDATE] Payment DTO (Data to Object) for use in PaymentAdapter

--- FrontendService/src/main/java/th/in/shopdi/FrontendService/DTO/Payment.java
+++ FrontendService/src/main/java/th/in/shopdi/FrontendService/DTO/Payment.java
@@ -1,22 +1,73 @@
 package th.in.shopdi.FrontendService.DTO;
 
 public class Payment {
-  private String token;
-  private long amount;
+  private long id;
 
-  public String getToken() {
-    return token;
+  private String holderName;
+
+  private String cardNumber;
+
+  private String expiredMonth;
+
+  private String expiredYear;
+
+  private String cvv;
+
+  private long userId;
+
+  public long getId() {
+    return id;
   }
 
-  public void setToken(String token) {
-    this.token = token;
+  public void setId(long id) {
+    this.id = id;
   }
 
-  public long getAmount() {
-    return amount;
+  public String getHolderName() {
+    return holderName;
   }
 
-  public void setAmount(long amount) {
-    this.amount = amount;
+  public void setHolderName(String holderName) {
+    this.holderName = holderName;
+  }
+
+  public String getCardNumber() {
+    return cardNumber;
+  }
+
+  public void setCardNumber(String cardNumber) {
+    this.cardNumber = cardNumber;
+  }
+
+  public String getExpiredMonth() {
+    return expiredMonth;
+  }
+
+  public void setExpiredMonth(String expiredMonth) {
+    this.expiredMonth = expiredMonth;
+  }
+
+  public String getExpiredYear() {
+    return expiredYear;
+  }
+
+  public void setExpiredYear(String expiredYear) {
+    this.expiredYear = expiredYear;
+  }
+
+  public String getCvv() {
+    return cvv;
+  }
+
+  public void setCvv(String cvv) {
+    this.cvv = cvv;
+  }
+
+  public long getUserId() {
+    return userId;
+  }
+
+  public void setUserId(long userId) {
+    this.userId = userId;
   }
 }
