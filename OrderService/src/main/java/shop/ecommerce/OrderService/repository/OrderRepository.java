673afe5e4c7de95f96599c81942658d94ec03ea8
PimIsariya <pim.paw.pitode@gmail.com>
2018-11-20 17:03:30 +0700

    [UPDATE] ย้ายไฟล์

--- OrderService/src/main/java/shop/ecommerce/OrderService/repository/OrderRepository.java
+++ OrderService/src/main/java/shop/ecommerce/OrderService/repository/OrderRepository.java
@@ -1,7 +1,9 @@
-package shop.ecommerce.OrderService;
+package shop.ecommerce.OrderService.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import shop.ecommerce.OrderService.model.Order;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
